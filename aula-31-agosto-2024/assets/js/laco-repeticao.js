d59bc90baeae60ce789faec618ba4be2ce8d9426
Gideão Ferreira <gideaoferreira@hotmail.com>
2024-10-05 10:58:12 -0300

    Pasta chata

---
+++ aula-31-agosto-2024/assets/js/laco-repeticao.js
@@ -0,0 +1,24 @@
+// = : Atribuição
+// == : Comparação de valor
+// === : Comparação de tipo e valor
+// != : Diferete
+// < > = Menor e Maior que.
+// Condicional E (&&)
+// Condicional OU (||)
+
+// User, Editor, Admin, Visitante
+
+let typeUser = "Admin";
+
+if (name === "User") {
+  //....
+} else {
+}
+
+for (let x = 1; x <= 10; x++) {
+  if (x >= 2 || x < 10) {
+    console.log("IF", x);
+  } else {
+    console.log("ELSE", x);
+  }
+}
